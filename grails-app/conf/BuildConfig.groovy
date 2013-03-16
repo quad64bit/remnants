59fbae7dab32adca7f7df584e45dd53caa40aa46
Steffen Gates <sgates@ctacorp.com>
2013-03-16 16:10:33 -0400

    Spring security added. Intellij files added to .gitignore

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -57,7 +57,7 @@ grails.project.dependency.resolution = {
         build ":tomcat:$grailsVersion"
 
         runtime ":database-migration:1.3.2"
-
+        compile ":spring-security-core:1.2.7.3"
         compile ':cache:1.0.1'
     }
 }
--- src/groovy/ActionList.groovy
+++ src/groovy/ActionList.groovy
@@ -1,4 +1,3 @@
 class ActionList{
 	def actions = []
-
 }
